d6b1b2fedbb8764c3ae0bdbd59e507aebd9231f0
Mikhail Metrikin <metrikin@gmail.com>
2017-02-17 15:59:01 -0800

    Dynamically generate char box's

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,20 +7,25 @@ function Character(name, hp, attack, counter) {
   // this.imgpath = "assets/images/" + imgpath;
 }
 
-// var charArray = [new Character("First", 180, 25, 45), new Character("Second", 180, 25, 45), new Character("Third", 180, 25, 45)];
+var charArray = [new Character("First", 180, 25, 45), new Character("Second", 180, 25, 45), new Character("Third", 180, 25, 45), new Character("Fourth", 180, 10, 45)];
 
 $(document).ready(function(){
 
-  var charArray = [new Character("First", 180, 25, 45), new Character("Second", 180, 25, 45), new Character("Third", 180, 25, 45)];
-
   for(var i = 0; i < charArray.length; i++){
+    // Create container for Character
     var charItem =  $("<li>");
+
+    // Create data variables for Characters
     charItem.attr("class", "character");
     charItem.attr("data-name", charArray[i].name);
     charItem.attr("data-hp", charArray[i].hp);
     charItem.attr("data-attack", charArray[i].attack);
     charItem.attr("data-counter", charArray[i].counter);
-    charItem.text(charArray[i].name);
+
+    // Place values in the container
+    charItem.append($("<p class='text-center'>").text(charArray[i].name));
+    // TODO: Add image with .img-responsive
+    charItem.append($("<p class='text-center'>").text("HP:" + charArray[i].hp + " Atk:" + charArray[i].attack + " Ctr:" + charArray[i].counter));
 
     $("#characters-list").append(charItem);
   }
