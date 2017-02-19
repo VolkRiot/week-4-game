12eecd4f65fbcb246b06eea06ee81f6f41e6ea5c
Mikhail Metrikin <metrikin@gmail.com>
2017-02-18 20:20:32 -0800

    Functionalized + add opponent lock-logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,29 +8,34 @@ function Character(name, hp, attack, counter) {
 }
 
 var $playerChar;
-var enemies = [];
+var opponent;
 var $instructionsParag = $("#instructions");
-
+var enemyLock = false;
 var charArray = [new Character("First", 180, 25, 45), new Character("Second", 180, 25, 45), new Character("Third", 180, 25, 45), new Character("Fourth", 180, 10, 45)];
 
-$(document).ready(function(){
+function generateChars(objList) {
 
-  for(var i = 0; i < charArray.length; i++){
+  for(var i = 0; i < objList.length; i++){
 
     var charItem =  $("<li class='character'>");
     charItem.attr("id", i+1);
 
-    for (var item in charArray[i]){
-      charItem.attr("data-" + item.toString(), charArray[i][item]);
+    for (var item in objList[i]){
+      charItem.attr("data-" + item.toString(), objList[i][item]);
     }
 
-    charItem.append($("<p class='text-center'>").text(charArray[i].name));
+    charItem.append($("<p class='text-center'>").text(objList[i].name));
 
     // TODO: Add image with .img-responsive
 
-    charItem.append($("<p class='text-center'>").text("HP:" + charArray[i].hp + " Atk:" + charArray[i].attack + " Ctr:" + charArray[i].counter));
+    charItem.append($("<p class='text-center'>").text("HP:" + objList[i].hp + " Atk:" + objList[i].attack + " Ctr:" + objList[i].counter));
     $("#characters-list").append(charItem);
   }
+}
+
+$(document).ready(function(){
+
+  generateChars(charArray);
 
   $(".character").on("click", function(){
     $playerChar = $(this);
@@ -41,9 +46,18 @@ $(document).ready(function(){
 
     // TODO: Enemies Logic
     $(".character").off().attr("class", "enemies enemy-characters");
-
+    $instructionsParag.text("Choose your opponent wisely");
   });
 
+  $("#characters-list").on('click', ".enemies", function(){
+
+      if(!enemyLock){
+        $(".player-enemies-container").append($(this));
+        opponent = charArray[parseInt($(this).attr('id') - 1)];
+        enemyLock = true;
+      }
+
+  });
 
 
 });
